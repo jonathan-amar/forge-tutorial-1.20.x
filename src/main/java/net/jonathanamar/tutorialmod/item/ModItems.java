dd3806eb8927e41cab36e655ca3b20b9b50e6555
Jonathan Amar <amar.jonathan00@gmail.com>
2023-12-03 20:41:54 -0500

    Added Metal Detector, item to detect rare ores at any y-level below selected block

--- src/main/java/net/jonathanamar/tutorialmod/item/ModItems.java
+++ src/main/java/net/jonathanamar/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jonathanamar.tutorialmod.item;
 
 import net.jonathanamar.tutorialmod.TutorialMod;
+import net.jonathanamar.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,12 +11,13 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
-
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
             () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
