dac6d79639cbd765bbf0087363cbead547b1650a
Jonathan Amar <amar.jonathan00@gmail.com>
2023-12-04 23:42:06 -0500

    DataGen complete, BEFORE DELETING JSON FILES

---
+++ src/main/java/net/jonathanamar/tutorialmod/dategen/loot/ModBlockLootTables.java
@@ -0,0 +1,40 @@
+package net.jonathanamar.tutorialmod.dategen.loot;
+
+import net.jonathanamar.tutorialmod.block.ModBlocks;
+import net.jonathanamar.tutorialmod.item.ModItems;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    protected ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        this.dropSelf(ModBlocks.SAPPHIRE_BLOCK.get());
+        this.dropSelf(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+        this.dropSelf(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+        this.dropSelf(ModBlocks.SOUND_BLOCK.get());
+
+        this.add(ModBlocks.SAPPHIRE_ORE.get(),
+                block -> createOreDrop(ModBlocks.SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+        this.add(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                block -> createOreDrop(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+        this.add(ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                block -> createOreDrop(ModBlocks.NETHER_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+        this.add(ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                block -> createOreDrop(ModBlocks.END_STONE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+}
