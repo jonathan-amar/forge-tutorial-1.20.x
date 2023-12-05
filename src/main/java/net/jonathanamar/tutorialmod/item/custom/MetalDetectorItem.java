91f96a24f687e639d298039bfaa0489e29f91ebe
Jonathan Amar <amar.jonathan00@gmail.com>
2023-12-04 21:02:39 -0500

    Added metal detector valuables tag

--- src/main/java/net/jonathanamar/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/jonathanamar/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.jonathanamar.tutorialmod.item.custom;
 
+import net.jonathanamar.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -61,6 +62,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state){
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.Metal_DETECTOR_VALUABLES);
     }
 }
